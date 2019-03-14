14e927ef14ad6cc33b8a1bb0bdf996c56fa2aab0
sharonshiri <sharonshiri@gmail.com>
2019-03-14 15:42:46 +0200

    start

---
+++ ex1/MissionContainer.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Excercise_1
+{
+    public delegate double FuncDelegate(double x);
+    public class FunctionsContainer
+    {
+       
+        private Dictionary<string, FuncDelegate> functionsDictionary = new Dictionary<string, FuncDelegate>();
+
+        public FuncDelegate this[string funcName]
+        {
+            get { return functionsDictionary[funcName]; }
+            set { functionsDictionary[funcName] = value; }
+        }
+    }
+}
---
+++ ex1/SingleMission.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Excercise_1
+{
+    public class SingleMission : IMission
+    {
+        private string myMissionName;
+        private FuncDelegate myFunc;
+
+        public SingleMission(string mission, FuncDelegate function)
+        {
+            myMissionName = mission;
+            myFunc = function;
+        }
+        
+        String Name { get => myMissionName;}
+        String Type { get => "Single"; }
+        
+        public double Calculate(double value)
+        {
+            return myFunc(value);
+        }
+    }
+}
